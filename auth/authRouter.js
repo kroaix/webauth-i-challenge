787ffeac90abb37e092116c96b621093ed8a8839
Katrina Roaix <kroaix@gmail.com>
2019-12-05 10:02:29 -0500

    MVP Complete

--- auth/authRouter.js
+++ auth/authRouter.js
@@ -1,5 +1,34 @@
-const router = require('express').Router();
+const router = require("express").Router();
 
-const Users = require('../users/usersModel.js')
+const bcrypt = require("bcryptjs");
+
+const Users = require("../users/usersModel.js");
+
+router.post("/register", async (req, res) => {
+  let user = req.body;
+  const hash = bcrypt.hashSync(user.password, 8);
+  user.password = hash;
+  try {
+    const newUser = await Users.add(user);
+    res.status(201).json(newUser);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.post("/login", async (req, res) => {
+  let { username, password } = req.body;
+  try {
+    const user = await Users.findBy({ username })
+    .first()
+    if (user && bcrypt.compareSync(password, user.password)) {
+      res.json({ message: `Welcome ${user.username}` });
+    } else {
+      res.status(401).json({ message: "Invalid Credentials" });
+    }
+  } catch {
+    res.status(500).json({ message: "You shall not pass!" });
+  }
+});
 
 module.exports = router;
--- users/usersRouter.js
+++ users/usersRouter.js
@@ -2,4 +2,13 @@ const router = require('express').Router();
 
 const Users = require('./usersModel.js');
 
+router.get('/', async (req, res) => {
+  try {
+    const users = await Users.find();
+    res.json(users);
+  } catch {
+    res.status(500).json({ message: "You shall not pass!" });
+  }
+})
+
 module.exports = router;
